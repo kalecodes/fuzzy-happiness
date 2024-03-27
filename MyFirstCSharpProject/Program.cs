5787ce78dd40c49c9893bb0946e682dfaa339888
kalecodes <kalen.wiley@gmail.com>
2024-03-27 19:59:28 -0400

    bool, operators, remainder, varKeyword

--- MyFirstCSharpProject/Program.cs
+++ MyFirstCSharpProject/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
-            //NumberDateTypes();
-            //TextBasedDataTypes();
+            NumberDateTypes();
+            TextBasedDataTypes();
             ConvertingTypes();
+            BooleanType();
+            Operators();
+            Remainder();
+            VarKeyword();
             
         }
 
@@ -45,7 +49,7 @@
             Console.WriteLine(decimal.MinValue);
             Console.WriteLine(decimal.MaxValue);
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
         static void TextBasedDataTypes()
@@ -57,7 +61,7 @@
             Console.WriteLine(letter);
             Console.WriteLine($"Your name is {name}, it starts with the letter {letter}.");
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
         static void ConvertingTypes()
@@ -86,6 +90,78 @@
             decimal money = Convert.ToDecimal(textMoney);
             Console.WriteLine(money);
             Console.WriteLine(money.GetType());
+        }
+
+        static void BooleanType()
+        {
+            bool value = false;
+            bool isMale = true;
+            Console.WriteLine(value);
+            Console.WriteLine(isMale);
+        }
+
+        static void Operators()
+        {
+            // + - * /
+
+            int age = 23;
+            age++;
+            Console.WriteLine(age);
+            age--;
+            Console.WriteLine(age);
+            age = age + 1;
+            Console.WriteLine(age);
+            age += 1;
+            Console.WriteLine(age);
+            age += 10;
+            Console.WriteLine(age);
+
+            int year = 2024;
+            year /= 2;
+            Console.WriteLine(year);
+
+            string name = "Abe";
+            name += " is programming";
+            Console.WriteLine(name);
+
+            // this does not work correctly because it is trying to add the unicode values together
+            char letter = 'a';
+            letter += 'b';
+            Console.WriteLine(letter);
+
+            // the ++ before the variable tells the compiler to execute the operation first, then return the value
+            // the ++ after returns the value first, and then executes the operation
+            int i = 0;
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+            Console.WriteLine(++i);
+
+        }
+
+        static void Remainder()
+        {
+            int firstNum = 10;
+            int secondNum = 3;
+            // This will truncate the result to another integer (3)
+            Console.WriteLine(firstNum / secondNum); // 3.333
+            // This is called the 'modulus operator', which gives us the remainder
+            Console.WriteLine(firstNum % secondNum); // 3r1
+
+            Console.WriteLine(1000 % 90);
+            Console.WriteLine(100 % 90);
+            Console.WriteLine(71 % 10);
+        }
+
+        static void VarKeyword()
+        {
+            // the compiler will figure out what type it needs to be based on the value it is initialized with
+            // best practive to only use var when it is very obvious what the type is going to be (to improve read time)
+            var age = 23;
+            Console.WriteLine(age.GetType());
+            var bigNumber = 90000L;
+            Console.WriteLine(bigNumber.GetType());
+            var negative = -55.2D;
+            Console.WriteLine(negative.GetType());
         }
     }
 }
