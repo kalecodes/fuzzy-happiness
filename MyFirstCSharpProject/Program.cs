815bbd7027eed28a98b4eabb5828babfa771353c
kalecodes <kalen.wiley@gmail.com>
2024-03-27 19:35:41 -0400

    string based types and type conversions

--- MyFirstCSharpProject/Program.cs
+++ MyFirstCSharpProject/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            //NumberDateTypes();
+            //TextBasedDataTypes();
+            ConvertingTypes();
+            
+        }
+
+        static void NumberDateTypes()
+        {
             //int x;
             //int y;
             //int z;
@@ -38,6 +46,46 @@
             Console.WriteLine(decimal.MaxValue);
 
             Console.ReadLine();
+        }
+
+        static void TextBasedDataTypes()
+        {
+            string name = "Aba";
+            char letter = 'A';
+
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
+            Console.WriteLine($"Your name is {name}, it starts with the letter {letter}.");
+
+            Console.ReadLine();
+        }
+
+        static void ConvertingTypes()
+        {
+            string textAge = "-23";
+            int age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+            Console.WriteLine(age.GetType());
+
+            string textBigNumber = "90000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(bigNumber.GetType());
+
+            string textNegative = "-55.2";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+            Console.WriteLine(negative.GetType());
+
+            string textPrecision = "5.0000001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+            Console.WriteLine(precision.GetType());
+
+            string textMoney = "14.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
+            Console.WriteLine(money.GetType());
         }
     }
 }
