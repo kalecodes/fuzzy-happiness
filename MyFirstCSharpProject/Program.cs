38affe83dc1635e24f6f780c97d68a845ff9d97b
kalecodes <kalen.wiley@gmail.com>
2024-03-26 21:19:19 -0400

    initial commit

---
+++ MyFirstCSharpProject/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+            //int z;
+
+            int x, y, z;
+            int a = 5, b = 10, c = 20;
+
+
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            long bigNumber = 8948498L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            float precision = 5.00001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.ReadLine();
+        }
+    }
+}
